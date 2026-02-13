f97a52e090a2dee328f9268677f3a01b6d014329
Mirikashitaku <loretocharle1@gmail.com>
2026-02-13 10:21:09 +0800

    pulled from kardo

--- resources/views/pages/registerUser/users.blade.php
+++ resources/views/pages/registerUser/users.blade.php
@@ -3,11 +3,12 @@
 @include('components.triggers.users-userstype-toast')
 @include('components.triggers.UsersModal')
 @vite(['resources/js/users.js'])
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
 
 
 
