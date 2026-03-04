f855ec3677f1ce510a87f7ff606054255bba0a02
Mirikashitaku <loretocharle1@gmail.com>
2026-03-04 09:34:29 +0800

    REFACTOR: modify vite.config.js file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,13 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         // Register your alias here inside the main chain
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'prevent-back' => \App\Http\Middleware\PreventBackHistory::class,
-        ]);
-    })
-
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
             'user_type' => UsertypeMiddleware::class,
         'single.session' => \App\Http\Middleware\SingleSessionMiddleware::class,
         ]);
